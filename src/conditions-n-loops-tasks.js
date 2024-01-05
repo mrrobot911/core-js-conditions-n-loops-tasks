05944d038dc78271046e39a7ca0fe8169d42bc68
mrrobot911 <kelwin2705@gmail.com>
2024-01-05 18:51:44 +0300

    feat: shuffle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -457,8 +457,21 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let answ = str;
+  for (let index = 0; index < iterations; index += 1) {
+    let newString = '';
+
+    for (let i = 0; i < str.length; i += 2) {
+      newString += answ[i];
+    }
+    for (let i = 1; i < str.length; i += 2) {
+      newString += answ[i];
+    }
+
+    answ = newString;
+  }
+  return answ;
 }
 
 /**
